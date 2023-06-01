bb1c0e86ce37a5ff98be33acc40fbccb983a6e84
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-05-31 21:55:57 -0300

    feat: configured typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response, response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
