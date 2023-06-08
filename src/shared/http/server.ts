81277884c1360a8538c818dd1be68bcf17bde13c
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-08 17:17:40 -0300

    feat: error handler adjustment

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response, response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use(
-  (error: Error, request: Request, reponse: Response, next: NextFunction) => {
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
         status: 'error',
