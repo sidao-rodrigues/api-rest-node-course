7d329c89dce596ad989cf58bbbe3be3fd1bc34a4
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-10 14:40:30 -0300

    feat: created user service

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userExists = await usersRepository.findByEmail(email);
+
+    if (userExists) {
+      throw new AppError('Email address already used.');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
