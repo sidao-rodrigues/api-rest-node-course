d558c3582fc0146d5dd22f144bb55543e8c2f3b7
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-10 14:59:06 -0300

    feat: created users routes and refactoring

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import UserController from '../controllers/UsersController';
+import { Segments, celebrate } from 'celebrate';
+import Joi from 'joi';
+
+const usersRouter = Router();
+const usersController = new UserController();
+
+usersRouter.get('/', usersController.index);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+export default usersRouter;
