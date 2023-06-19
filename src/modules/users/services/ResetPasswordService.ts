d29883cdcc77f697350affc37d22712dbbb065c4
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-19 13:51:24 -0300

    feat: created send forgot password service and reset password service
    and added date-fns dependency

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,41 @@
+import { getCustomRepository } from 'typeorm';
+import { isAfter, addHours } from 'date-fns';
+import { hash } from 'bcryptjs';
+import AppError from '@shared/errors/AppError';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokenRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokenRepository);
+
+    const userToken = await userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User Token does not exists.');
+    }
+
+    const user = await usersRepository.findById(userToken.userId);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const tokenCreatedAt = userToken.createdAt;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expried');
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
+
+export default ResetPasswordService;
