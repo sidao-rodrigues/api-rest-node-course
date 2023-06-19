92796cda1740b3d95c79674291512a42097ea411
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-19 15:14:42 -0300

    feat: created show profile service

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  userId: string;
+}
+
+class ShowProfileService {
+  public async execute({ userId }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(userId);
+
+    if (!user) {
+      throw new AppError('User not found.', 404);
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
