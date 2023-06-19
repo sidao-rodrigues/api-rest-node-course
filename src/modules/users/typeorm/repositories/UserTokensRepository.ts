3bb4a3752cad925ec452b780e270954c026ac437
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-19 13:36:30 -0300

    feat: created user_token repository

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,25 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserToken from '../entities/UserToken';
+
+@EntityRepository(UserToken)
+class UserTokenRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.findOne({
+      where: {
+        token,
+      },
+    });
+    return userToken;
+  }
+
+  public async generate(userId: string): Promise<UserToken | undefined> {
+    const userToken = this.create({
+      userId,
+    });
+    await this.save(userToken);
+
+    return userToken;
+  }
+}
+
+export default UserTokenRepository;
