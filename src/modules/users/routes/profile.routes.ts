310f89c43a67e1b4f39670e6a5ceb6dd92670013
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-19 15:35:44 -0300

    feat: created user profile router

---
+++ src/modules/users/routes/profile.routes.ts
@@ -0,0 +1,33 @@
+import isAutenticated from '@shared/http/middlewares/isAuthenticated';
+import { Segments, celebrate } from 'celebrate';
+import { Router } from 'express';
+import Joi from 'joi';
+import ProfileController from '../controllers/ProfileController';
+
+const profileRouter = Router();
+const profileController = new ProfileController();
+
+profileRouter.use(isAutenticated);
+
+profileRouter.get('/', profileController.show);
+
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      oldPassword: Joi.string(),
+      password: Joi.string().optional(),
+      passwordConfirmation: Joi.string()
+        .valid(Joi.ref('password'))
+        .when('password', {
+          is: Joi.exist(),
+          then: Joi.required(),
+        }),
+    },
+  }),
+  profileController.update,
+);
+
+export default profileRouter;
