1f29de6f6499bd1b19acdecd24254a47ff9d68b6
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-19 13:32:34 -0300

    feat: created user_token entity

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('user_tokens')
+class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column({ name: 'user_id' })
+  userId: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @CreateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
+
+export default UserToken;
