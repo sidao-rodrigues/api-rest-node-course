db6a9977978b57b4b80f71b8ac548408643ede5e
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-19 14:10:59 -0300

    feat: created reset password route

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { Segments, celebrate } from 'celebrate';
 import Joi from 'joi';
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
 
 const passwordRouter = Router();
 const forgotPasswordCntroller = new ForgotPasswordController();
+const resetPasswordCntroller = new ResetPasswordController();
 
 passwordRouter.post(
   '/forgot',
@@ -20,10 +22,12 @@ passwordRouter.post(
   '/reset',
   celebrate({
     [Segments.BODY]: {
-      email: Joi.string().email().required(),
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      passwordConfirmation: Joi.string().required().valid(Joi.ref('password')),
     },
   }),
-  forgotPasswordCntroller.create,
+  resetPasswordCntroller.create,
 );
 
 export default passwordRouter;
