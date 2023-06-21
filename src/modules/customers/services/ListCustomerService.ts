20b5a46111cde1ef637581b54a8dc751c5b6a6b1
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-21 12:10:20 -0300

    feat: added typeorm-paginate dependency and customer adjustments

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -2,13 +2,37 @@ import { getCustomRepository } from 'typeorm';
 import Customer from '../typeorm/entities/Customer';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 
+//ANALISAR COMO PODE SER UTILIZADO MELHOR
+type CamelCase<S extends string> =
+  S extends `${infer P1}_${infer P2}${infer P3}`
+    ? `${Lowercase<P1>}${Uppercase<P2>}${CamelCase<P3>}`
+    : Lowercase<S>;
+
+type KeysToCamelCase<T> = {
+  [K in keyof T as CamelCase<string & K>]: T[K] extends `{}`
+    ? KeysToCamelCase<T[K]>
+    : T[K];
+};
+//ANALISAR COMO PODE SER UTILIZADO MELHOR
+
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
+
 class ListCustomerService {
-  public async execute(): Promise<Customer[]> {
+  public async execute(): Promise<IPaginateCustomer> {
     const customersRepository = getCustomRepository(CustomersRepository);
 
-    const customers = await customersRepository.find();
+    const customers = await customersRepository.createQueryBuilder().paginate();
 
-    return customers;
+    return customers as IPaginateCustomer;
   }
 }
 
