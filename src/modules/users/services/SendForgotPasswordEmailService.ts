b9cca1e52d367eb9d8e4487688472ccbe31bddfd
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-21 14:22:54 -0300

    feat: adjustments send email by environment

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,9 +1,11 @@
 import { getCustomRepository } from 'typeorm';
+import mailConfig from '@config/mail/mail';
 import AppError from '@shared/errors/AppError';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokenRepository from '../typeorm/repositories/UserTokensRepository';
 import EtherealMail from '@config/mail/EtherealMail';
 import path from 'path';
+import SESMail from '@config/mail/SESMail';
 
 interface IRequest {
   email: string;
@@ -29,6 +31,23 @@ class SendForgotPasswordEmailService {
       'forgot_password.hbs',
     );
 
+    if (mailConfig.driver === 'ses') {
+      await SESMail.sendMail({
+        to: {
+          name: user.name,
+          email: user.email,
+        },
+        subject: '[API Vendas] Recuperação de Senha',
+        templateData: {
+          file: forgotPasswordTemplate,
+          variables: {
+            name: user.name,
+            link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+          },
+        },
+      });
+      return;
+    }
     await EtherealMail.sendMail({
       to: {
         name: user.name,
