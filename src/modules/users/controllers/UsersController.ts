226c8f2b531b537403bf0160d5ff7325f68ab8dc
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-21 12:40:07 -0300

    feat: added class-transformer dependency and configuration

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import ListUserService from '../services/ListUserService';
 import CreateUserService from '../services/CreateUserService';
+import { instanceToInstance } from 'class-transformer';
 
 export default class UserController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -8,7 +9,7 @@ export default class UserController {
 
     const users = await listUser.execute();
 
-    return response.json(users);
+    return response.json(instanceToInstance(users));
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
@@ -22,6 +23,6 @@ export default class UserController {
       password,
     });
 
-    return response.status(201).json(user);
+    return response.status(201).json(instanceToInstance(user));
   }
 }
