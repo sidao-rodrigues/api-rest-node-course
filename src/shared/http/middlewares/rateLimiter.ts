f780d921fe7f1f82d557add7f8b7b890dae28792
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-21 13:51:22 -0300

    fix: adjustments rate-limiter

--- src/shared/http/middlewares/rateLimiter.ts
+++ src/shared/http/middlewares/rateLimiter.ts
@@ -1,20 +1,8 @@
-import { Request, Response, NextFunction, response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import redis from 'redis';
+//import Redis from 'ioredis'; v4 redis
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 import AppError from '@shared/errors/AppError';
-
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASS,
-});
-
-const limiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: 'ratelimit',
-  points: 5, //quantidade de requisições por ip em x (duration)
-  duration: 1, //second
-});
 
 export default async function rateLimiter(
   request: Request,
@@ -22,6 +10,20 @@ export default async function rateLimiter(
   next: NextFunction,
 ): Promise<void> {
   try {
+    // const redisClient = new Redis({ v4 redis
+    const redisClient = redis.createClient({
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+      password: process.env.REDIS_PASS,
+    });
+
+    const limiter = new RateLimiterRedis({
+      storeClient: redisClient,
+      keyPrefix: 'ratelimit',
+      points: 5, //quantidade de requisições por ip em x (duration)
+      duration: 1, //second
+    });
+
     await limiter.consume(request.ip);
     return next();
   } catch (err) {
