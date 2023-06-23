ce8b69b7c586386741b9f3ad6677f9cbcffca5a9
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-23 20:18:15 -0300

    fix: hash provider adjustments

--- src/modules/users/providers/HashProvider/index.ts
+++
@@ -1,5 +0,0 @@
-import { container } from 'tsyringe';
-import BcryptHashProvider from './implementations/BcryptHashProvider';
-import { IHashProvider } from './models/IHashProvider';
-
-container.registerSingleton<IHashProvider>('HashProvider', BcryptHashProvider);
