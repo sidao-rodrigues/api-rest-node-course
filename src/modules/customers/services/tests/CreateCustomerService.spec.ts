fa037959e63d5b3be7a3578d7d304b972f774701
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2023-06-23 17:39:11 -0300

    feat: createcustomerservice test refactoring

--- src/modules/customers/services/tests/CreateCustomerService.spec.ts
+++ src/modules/customers/services/tests/CreateCustomerService.spec.ts
@@ -3,12 +3,16 @@ import FakeCustomersRepository from '@modules/customers/domain/repositories/fake
 import CreateCustomerService from '../CreateCustomerService';
 import AppError from '@shared/errors/AppError';
 
+let fakeCustomersRepository: FakeCustomersRepository;
+let createCustumer: CreateCustomerService;
+
 describe('CreateCustomer', () => {
+  beforeEach(() => {
+    fakeCustomersRepository = new FakeCustomersRepository();
+    createCustumer = new CreateCustomerService(fakeCustomersRepository);
+  });
+
   it('should be able to create a new customer', async () => {
-    const fakeCustomersRepository = new FakeCustomersRepository();
-
-    const createCustumer = new CreateCustomerService(fakeCustomersRepository);
-
     const customer = await createCustumer.execute({
       name: 'Sidney Rodrigues',
       email: 'teste@teste.com.br',
@@ -18,10 +22,6 @@ describe('CreateCustomer', () => {
   });
 
   it('should not be able to create twot customers with eh same email', async () => {
-    const fakeCustomersRepository = new FakeCustomersRepository();
-
-    const createCustumer = new CreateCustomerService(fakeCustomersRepository);
-
     await createCustumer.execute({
       name: 'Sidney Rodrigues',
       email: 'teste@teste.com.br',
